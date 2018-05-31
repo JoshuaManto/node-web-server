7d355a9de5ad53808ce08bcc3c7af9495eb789d1
JoshuaManto <joshuamanto@yahoo.com>
2018-05-31 15:45:54 +0800

    Add port customization for Heroku and local

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port =process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -79,7 +81,7 @@ app.get('/bad', (req, res) =>
 
 // will be changed depending on the server
 // can take 2nd arguments. It is a function
-app.listen(3000, () =>
+app.listen(port, () =>
 {
-  console.log('Server is up on port 3000');
+  console.log(`Server is up on port ${port}`);
 });
